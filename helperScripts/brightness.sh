9b4b086fa8346eade0cc8ccdf2e0c275adf74d33
G4bbix <winkiway@gmail.com>
2021-12-23 21:12:01 +0100

    Inital commit with brightness.sh

---
+++ helperScripts/brightness.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+# Set brightness of all screens
+
+BRIGHTNESS="$(xrandr --prop --verbose | grep Bright | head -1  | cut -d " " -f2)"
+OPERATOR="${1:0:1}"
+VALUE="${1:1:$}"
+
+if [ "$OPERATOR" = "+" ] ; then
+  for SCREEN in $(xrandr | grep -w connected | cut -d " " -f1) ; do
+    xrandr --output "$SCREEN" --brightness $(( BRIGHTNESS + VALUE))
+  done
+elif [ "$OPERATOR" = "-" ] ; then
+  if [ "$(echo "$BRIGHTNESS - $VALUE > 0.10" | bc)" = 1 ] ; then
+    for SCREEN in $(xrandr | grep -w connected | cut -d " " -f1) ; do
+      xrandr --output "$SCREEN" --brightness $(( BRIGHTNESS - VALUE))
+    done
+  fi
+fi
