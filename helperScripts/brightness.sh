586fee99a9cd409ffe7ae72d740289a64cf4dc80
G4bbix <winkiway@gmail.com>
2022-04-26 15:37:21 +0200

    Added check to prevent going over 100%

--- helperScripts/brightness.sh
+++ helperScripts/brightness.sh
@@ -6,7 +6,7 @@ BRIGHTNESS="$(xrandr --prop --verbose | grep Bright | head -1  | cut -d " " -f2)
 OPERATOR="${1:0:1}"
 VALUE="${1:1:$}"
 
-if [ "$OPERATOR" = "+" ] ; then
+if [ "$OPERATOR" = "+" ] && [ "$BRIGHTNESS" != "1.0" ] ; then
   for SCREEN in $(xrandr | grep -w connected | cut -d " " -f1) ; do
     xrandr --output "$SCREEN" --brightness $(( BRIGHTNESS + VALUE))
   done
