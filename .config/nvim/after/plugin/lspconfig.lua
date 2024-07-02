c17593201b158d21c054354cbe2cf421d6b125f3
gabriel.winkler <gabriel.winkler@bpm.ch>
2024-07-02 08:49:30 +0200

    Removed sonarlint

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -174,7 +174,6 @@ require("mason").setup()
 	"luacheck",
 	"shellcheck",
 	"shfmt",
-	"sonarlint-language-server",
 	"hadolint",
 	"ansible-lint",
 	"vint",
@@ -292,35 +291,4 @@ if not configs.golangcilsp then
 end
 lspconfig.golangci_lint_ls.setup({
 	filetypes = { "go", "gomod" },
-})
-
-require("sonarlint").setup({
-	server = {
-		cmd = {
-			"sonarlint-language-server",
-			-- Ensure that sonarlint-language-server uses stdio channel
-			"-stdio",
-			"-analyzers",
-			-- paths to the analyzers you need, using those for python and java in this example
-			vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
-			vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
-			vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
-		},
-		-- All settings are optional
-		settings = {
-			-- The default for sonarlint is {}, this is just an example
-			sonarlint = {
-				rules = {
-					-- ["typescript:S101"] = { level = "on", parameters = { format = "^[A-Z][a-zA-Z0-9]*$" } },
-					-- ["typescript:S103"] = { level = "on", parameters = { maximumLineLength = 180 } },
-					-- ["typescript:S106"] = { level = "on" },
-					-- ["typescript:S107"] = { level = "on", parameters = { maximumFunctionParameters = 7 } },
-				},
-			},
-		},
-	},
-	filetypes = {
-		"python",
-		"go",
-	},
 })
