df5cfb1f87d7f59efda7a9c131a13e2b0a017e29
gabriel.winkler <gabriel.winkler@bpm.ch>
2024-12-18 11:36:36 +0100

    Genesis

---
+++ .config/nvim/after/plugin/dap.lua
@@ -0,0 +1,7 @@
+local dap = require('dap')
+  dap.adapters.python = {
+    type = 'executable';
+    command = os.getenv('HOME') .. '/.virtualenvs/tools/bin/python';
+    args = { '-m', 'debugpy.adapter' };
+  }
+
