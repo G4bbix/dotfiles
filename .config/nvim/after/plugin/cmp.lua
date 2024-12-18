ea1f1fe616279068ca2193d19425732306613ecd
gabriel.winkler <gabriel.winkler@bpm.ch>
2024-12-18 11:32:48 +0100

    Fixed autocompletion

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -12,12 +12,58 @@ cmp.setup({
 			vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
 		end,
 	},
+	completion = { completeopt = 'menu,menuone,noinsert' },
 	mapping = cmp.mapping.preset.insert({
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+		-- For an understanding of why these mappings were
+		-- chosen, you will need to read `:help ins-completion`
+		--
+		-- No, but seriously. Please read `:help ins-completion`, it is really good!
+			-- Select the [n]ext item
+			['<C-n>'] = cmp.mapping.select_next_item(),
+			-- Select the [p]revious item
+			['<C-p>'] = cmp.mapping.select_prev_item(),
+
+			-- Scroll the documentation window [b]ack / [f]orward
+			['<C-b>'] = cmp.mapping.scroll_docs(-4),
+			['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+			-- Accept ([y]es) the completion.
+			--  This will auto-import if your LSP supports it.
+			--  This will expand snippets if the LSP sent a snippet.
+			['<C-y>'] = cmp.mapping.confirm { select = true },
+
+			-- If you prefer more traditional completion keymaps,
+			-- you can uncomment the following lines
+			--['<CR>'] = cmp.mapping.confirm { select = true },
+			--['<Tab>'] = cmp.mapping.select_next_item(),
+			--['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+			-- Manually trigger a completion from nvim-cmp.
+			--  Generally you don't need this, because nvim-cmp will display
+			--  completions whenever it has completion options available.
+			['<C-Space>'] = cmp.mapping.complete {},
+
+			-- Think of <c-l> as moving to the right of your snippet expansion.
+			--  So if you have a snippet that's like:
+			--  function $name($args)
+			--    $body
+			--  end
+			--
+			-- <c-l> will move you to the right of each of the expansion locations.
+			-- <c-h> is similar, except moving you backwards.
+			['<C-l>'] = cmp.mapping(function()
+				if luasnip.expand_or_locally_jumpable() then
+					luasnip.expand_or_jump()
+				end
+			end, { 'i', 's' }),
+			['<C-h>'] = cmp.mapping(function()
+				if luasnip.locally_jumpable(-1) then
+					luasnip.jump(-1)
+				end
+			end, { 'i', 's' }),
+
 	}),
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
