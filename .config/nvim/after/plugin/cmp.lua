c8d15a23d850410aa2e4b6ac5c5a9bd5633d2dee
G4bbix <winkiway@gmail.com>
2025-11-21 09:52:58 +0100

    Switch back to native autocompletion in cmd mode

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -99,15 +99,15 @@ cmp.setup.cmdline({ '/', '?' }, {
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = 'path' }
-	}, {
-		{ name = 'cmdline' }
-	}),
-	matching = { disallow_symbol_nonprefix_matching = false }
-})
+-- cmp.setup.cmdline(':', {
+-- 	mapping = cmp.mapping.preset.cmdline(),
+-- 	sources = cmp.config.sources({
+-- 		{ name = 'path' }
+-- 	}, {
+-- 		{ name = 'cmdline' }
+-- 	}),
+-- 	matching = { disallow_symbol_nonprefix_matching = false }
+-- })
 
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities() --nvim-cmp
