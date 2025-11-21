faa905618830b65da7dad6166c140b4d235333b1
G4bbix <winkiway@gmail.com>
2025-11-21 09:57:50 +0100

    Added shortcut for new tab

--- .config/nvim/lua/g4bbix/mappings.lua
+++ .config/nvim/lua/g4bbix/mappings.lua
@@ -21,6 +21,8 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+vim.keymap.set("n", "<leader>nt", ":tabnew<CR>", { desc = "Open new tab" })
+
 vim.keymap.set("n", "<leader>ww", function()
 	vim.opt.wrap = not vim.opt.wrap._value
 end, { desc = "Toggle wrapping" })
