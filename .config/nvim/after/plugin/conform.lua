c1a61c54416c14fe24db0a74327dbcc70dbb90e9
G4bbix <nosuchuser@nosuchdomain.ch>
2026-03-11 08:12:06 +0100

    Removed autoflake and autopep

--- .config/nvim/after/plugin/conform.lua
+++ .config/nvim/after/plugin/conform.lua
@@ -2,7 +2,7 @@ require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
 		go = { "golangci-lint" },
-    python = { "ruff_format", "ruff_fix", "autopep8", "autoflake" },
+    python = { "ruff_format", "ruff_fix" },
 		sh = { "shfmt" },
     javascript = { "eslint_d" },
     json =  { "fixjson", "jq" },
