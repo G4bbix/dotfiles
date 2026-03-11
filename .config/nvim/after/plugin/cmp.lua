bac5e1c34d0ba9ebe2ca1863782d1d6c89ac4efb
G4bbix <nosuchuser@nosuchdomain.ch>
2026-03-11 08:11:41 +0100

    Formatting and disable arrows for navigation in suggestions

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -1,6 +1,6 @@
 -- See `:help cmp`
-local cmp = require('cmp')
-local lspkind = require('lspkind')
+local cmp = require("cmp")
+local lspkind = require("lspkind")
 
 cmp.setup({
 	snippet = {
@@ -12,71 +12,79 @@ cmp.setup({
 			vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
 		end,
 	},
-	completion = { completeopt = 'menu,menuone,noinsert' },
+	completion = { completeopt = "menu,menuone,noinsert" },
 	mapping = cmp.mapping.preset.insert({
-		['<C-e>'] = cmp.mapping.abort(),
+		["<C-e>"] = cmp.mapping.abort(),
 
 		-- For an understanding of why these mappings were
 		-- chosen, you will need to read `:help ins-completion`
 		--
 		-- No, but seriously. Please read `:help ins-completion`, it is really good!
-			-- Select the [n]ext item
-			['<C-n>'] = cmp.mapping.select_next_item(),
-			-- Select the [p]revious item
-			['<C-p>'] = cmp.mapping.select_prev_item(),
+		-- Select the [n]ext item
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		-- Select the [p]revious item
+		["<C-p>"] = cmp.mapping.select_prev_item(),
 
-			-- Scroll the documentation window [b]ack / [f]orward
-			['<C-b>'] = cmp.mapping.scroll_docs(-4),
-			['<C-f>'] = cmp.mapping.scroll_docs(4),
+		-- Scroll the documentation window [b]ack / [f]orward
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
 
-			-- Accept ([y]es) the completion.
-			--  This will auto-import if your LSP supports it.
-			--  This will expand snippets if the LSP sent a snippet.
-			['<C-y>'] = cmp.mapping.confirm { select = true },
+		-- Accept ([y]es) the completion.
+		--  This will auto-import if your LSP supports it.
+		--  This will expand snippets if the LSP sent a snippet.
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
 
-			-- If you prefer more traditional completion keymaps,
-			-- you can uncomment the following lines
-			--['<CR>'] = cmp.mapping.confirm { select = true },
-			--['<Tab>'] = cmp.mapping.select_next_item(),
-			--['<S-Tab>'] = cmp.mapping.select_prev_item(),
+		-- If you prefer more traditional completion keymaps,
+		-- you can uncomment the following lines
+		--['<CR>'] = cmp.mapping.confirm { select = true },
+		--['<Tab>'] = cmp.mapping.select_next_item(),
+		--['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
-			-- Manually trigger a completion from nvim-cmp.
-			--  Generally you don't need this, because nvim-cmp will display
-			--  completions whenever it has completion options available.
-			['<C-Space>'] = cmp.mapping.complete {},
+		-- Manually trigger a completion from nvim-cmp.
+		--  Generally you don't need this, because nvim-cmp will display
+		--  completions whenever it has completion options available.
+		["<C-Space>"] = cmp.mapping.complete({}),
 
-			-- Think of <c-l> as moving to the right of your snippet expansion.
-			--  So if you have a snippet that's like:
-			--  function $name($args)
-			--    $body
-			--  end
-			--
-			-- <c-l> will move you to the right of each of the expansion locations.
-			-- <c-h> is similar, except moving you backwards.
-			['<C-l>'] = cmp.mapping(function()
-				if luasnip.expand_or_locally_jumpable() then
-					luasnip.expand_or_jump()
-				end
-			end, { 'i', 's' }),
-			['<C-h>'] = cmp.mapping(function()
-				if luasnip.locally_jumpable(-1) then
-					luasnip.jump(-1)
-				end
-			end, { 'i', 's' }),
+		-- Think of <c-l> as moving to the right of your snippet expansion.
+		--  So if you have a snippet that's like:
+		--  function $name($args)
+		--    $body
+		--  end
+		--
+		-- <c-l> will move you to the right of each of the expansion locations.
+		-- <c-h> is similar, except moving you backwards.
+		["<C-l>"] = cmp.mapping(function()
+			if luasnip.expand_or_locally_jumpable() then
+				luasnip.expand_or_jump()
+			end
+		end, { "i", "s" }),
+		["<C-h>"] = cmp.mapping(function()
+			if luasnip.locally_jumpable(-1) then
+				luasnip.jump(-1)
+			end
+		end, { "i", "s" }),
 
+		["<Down>"] = cmp.mapping(function(fallback)
+			cmp.close()
+			fallback()
+		end, { "i" }),
+		["<Up>"] = cmp.mapping(function(fallback)
+			cmp.close()
+			fallback()
+		end, { "i" }),
 	}),
 	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
+		{ name = "nvim_lsp" },
 		-- { name = 'vsnip' }, -- For vsnip users.
 		-- { name = 'luasnip' }, -- For luasnip users.
 		-- { name = 'ultisnips' }, -- For ultisnips users.
 		-- { name = 'snippy' }, -- For snippy users.
 	}, {
-		{ name = 'buffer' },
+		{ name = "buffer" },
 	}),
 	formatting = {
-    format = lspkind.cmp_format(),
-  },
+		format = lspkind.cmp_format(),
+	},
 })
 
 -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
@@ -88,14 +96,15 @@ cmp.setup({
 		{ name = 'buffer' },
 	})
 })
-require("cmp_git").setup() ]]--
+require("cmp_git").setup() ]]
+--
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
+cmp.setup.cmdline({ "/", "?" }, {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = {
-		{ name = 'buffer' }
-	}
+		{ name = "buffer" },
+	},
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
@@ -109,32 +118,35 @@ cmp.setup.cmdline({ '/', '?' }, {
 -- 	matching = { disallow_symbol_nonprefix_matching = false }
 -- })
 
-
-local capabilities = require('cmp_nvim_lsp').default_capabilities() --nvim-cmp
+local capabilities = require("cmp_nvim_lsp").default_capabilities() --nvim-cmp
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
+	local function buf_set_option(...)
+		vim.api.nvim_buf_set_option(bufnr, ...)
+	end
 
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 end
 
-local nvim_lsp = require('lspconfig')
+-- local nvim_lsp = vim.lsp.config()
 
-nvim_lsp['gopls'].setup{
-  cmd = {'gopls'},
-  -- on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    gopls = {
-      experimentalPostfixCompletions = true,
-      analyses = {
-        unusedparams = true,
-        shadow = true,
-      },
-      staticcheck = true,
-    },
-  },
-  init_options = {
-    usePlaceholders = true,
-  }
-}
+-- nvim_lsp['gopls'].setup{
+--   cmd = {'gopls'},
+--   -- on_attach = on_attach,
+--   capabilities = capabilities,
+--   settings = {
+--     gopls = {
+--       experimentalPostfixCompletions = true,
+--       analyses = {
+--         unusedparams = true,
+--         shadow = true,
+--       },
+--       staticcheck = true,
+--     },
+--   },
+--   init_options = {
+--     usePlaceholders = true,
+--   }
+-- }
