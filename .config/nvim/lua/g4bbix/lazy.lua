d15cf5851d7bd08e252e3fa1dcfa4e901e41af59
G4bbix <nosuchuser@nosuchdomain.ch>
2026-03-26 10:47:13 +0100

    Added opencode

--- .config/nvim/lua/g4bbix/lazy.lua
+++ .config/nvim/lua/g4bbix/lazy.lua
@@ -142,4 +142,33 @@ require("lazy").setup({
 		end,
 		ft = { "markdown" },
 	},
+	{
+		"nickjvandyke/opencode.nvim",
+		version = "*", -- Latest stable release
+		dependencies = {
+			{
+				-- `snacks.nvim` integration is recommended, but optional
+				---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
+				"folke/snacks.nvim",
+				optional = true,
+				opts = {
+					input = {}, -- Enhances `ask()`
+					picker = { -- Enhances `select()`
+						actions = {
+							opencode_send = function(...)
+								return require("opencode").snacks_picker_send(...)
+							end,
+						},
+						win = {
+							input = {
+								keys = {
+									["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
 })
